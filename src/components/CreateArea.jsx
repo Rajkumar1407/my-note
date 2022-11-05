b0d1590623432a9508789d990d769d168f0805a9
Rajkumar1407 <raj14kumar2002@gmail.com>
2022-11-05 21:53:48 +0530

    New files changed

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -4,7 +4,6 @@ import { IoIosAdd } from "react-icons/io";
 
 function CreateArea({onAdd}) {
     const [isExpanded, setExpanded] = useState(false);
-
     const [note, setNote] = useState({
         title: "",
         content: "",
@@ -24,6 +23,7 @@ function CreateArea({onAdd}) {
         setExpanded(true);
       }
 
+
       function submitButton(event){
         onAdd(note);
         setNote({
@@ -32,18 +32,38 @@ function CreateArea({onAdd}) {
         });
         event.preventDefault();
       }
+      
+      function fileUpload(e){
+        const file = e.target.files[0];
+        getBase64(file).then(base64 => {
+          localStorage["fileBase64"] = base64;
+          console.debug("file stored", base64);
+        });
+      }
 
-    return (
+      const getBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = () => resolve(reader.result);
+          reader.onerror = error => reject(error);
+          reader.readAsDataURL(file);
+        });
+      }
+
+      return (
         <div>
             <form>
                 {isExpanded && (
-                  <input type="text" 
+                  <input type="text"
                     value={note.title} 
                     placeholder="Create topic" 
                     name="title" 
                     onChange={handleChange} 
                   />
                 )}
+                <p>
+                  <input type="file" id="imageFile" onChange= {fileUpload} ></input>
+                </p>
                 <p>
                   <textarea 
                     name="content" 
@@ -60,6 +80,7 @@ function CreateArea({onAdd}) {
             </form>
         </div>
     );
+  
 }
 
 export default CreateArea;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,6 +6,7 @@ function Note({ title, content, onDelete, id }) {
     return (
         <div className="note">
             <h2>{title}</h2>
+            <img src={localStorage.getItem('fileBase64')}></img>
             <p>{content}</p>
             <button onClick={() => onDelete(id)}>
                 <MdDelete size={25} />
